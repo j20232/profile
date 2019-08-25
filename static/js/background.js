b7f766a97c1df9158af095e1569a85a5d43b2326
mocobt <mocobt@gmail.com>
2019-08-25 20:24:33 +0900

    Add marbles

--- static/js/background.js
+++ static/js/background.js
@@ -1,18 +1,5 @@
 window.onload = init();
 function init(){
-  var getDevice = (function(){
-    var ua = navigator.userAgent;
-    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
-        return 'sp';
-    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
-        return 'tab';
-    }else{
-        return 'other';
-    }
-  })();
-  if( getDevice == 'tab'  || getDevice== 'sp')
-    return;
-
   canvas = document.getElementById('canvas');
   context = canvas.getContext('2d');
   canvas.width = window.innerWidth ;
